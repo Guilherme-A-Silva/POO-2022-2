424613d408a84e14b2df46f66867629ee2d53de4
Guilherme A <68473916+Guilherme-A-Silva@users.noreply.github.com>
2022-12-23 11:32:02 -0300

    14.19

--- 14.19/ConversorDeData.java
+++ 14.19/ConversorDeData.java
@@ -12,25 +12,14 @@ import java.text.ParseException;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
-
-
 public class ConversorDeData {
-    public static void main(String[] args) throws ParseException {
-       
-
-        
-        
+    public static void main(String[] args) throws ParseException {  
             String EntradaDeData = JOptionPane.showInputDialog(null, "Insira a data no formato Mes/Dia/Ano.");
-
-            JOptionPane.showMessageDialog(null, EntradaDeData,  "Data inicial", 1);
-            
+            JOptionPane.showMessageDialog(null, EntradaDeData,  "Data inicial", 1);         
             SimpleDateFormat FormatoInicial = new SimpleDateFormat("MM/dd/yyyy");
             Date Data = FormatoInicial.parse(EntradaDeData);
             FormatoInicial.applyPattern("MMMM dd, yyyy");
-            String NovoFormato = FormatoInicial.format(Data);
-            
-            JOptionPane.showMessageDialog(null, NovoFormato, "Nova data",1);
-       
-            
+            String NovoFormato = FormatoInicial.format(Data);          
+            JOptionPane.showMessageDialog(null, NovoFormato, "Nova data",1);            
         }
     }
